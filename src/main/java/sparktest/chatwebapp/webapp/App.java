9cd53e639a65a8ba1417f4269d99dfd2b3dd42de
Rafael-s-costa <rafael.s.costa96@gmail.com>
2019-06-08 19:37:16 +0100

    heroku deployment ready

--- src/main/java/sparktest/chatwebapp/webapp/App.java
+++ src/main/java/sparktest/chatwebapp/webapp/App.java
@@ -15,7 +15,16 @@ public class App {
     static int nextUserNumber = 1; //Used for creating the next username
 
     public static void main(String[] args) {
+    	port(getHerokuAssignedPort());
     	webSocket("/chat", ChatEndpoint.class);
         init();
+    }
+    
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
