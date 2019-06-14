d73e7b214658f59b387f3e328028b937dfe809f2
Rafael-s-costa <rafael.s.costa96@gmail.com>
2019-06-15 00:12:40 +0100

    Added logging to chatendpoint

--- src/main/java/sparktest/chatwebapp/resources/ChatEndpoint.java
+++ src/main/java/sparktest/chatwebapp/resources/ChatEndpoint.java
@@ -13,6 +13,9 @@ import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import org.json.JSONObject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import sparktest.chatwebapp.pojo.MessageDTO;
 import sparktest.chatwebapp.webapp.App;
 
@@ -21,21 +24,25 @@ public class ChatEndpoint {
 
     private Session session;
     private String sender, msg;
+    final Logger logger = LoggerFactory.getLogger(ChatEndpoint.class);
 
     @OnWebSocketConnect
     public void onConnect(Session user) throws Exception {
     	String username = "User" + App.nextUserNumber++;
+    	logger.info("New user connected");
     	App.userUsernameMap.put(user, username);
     	App.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
     }
 
     @OnWebSocketMessage
     public void onMessage(Session user, String message) throws IOException {
+    	logger.info("Message " + "message");
     	App.broadcastMessage(sender = App.userUsernameMap.get(user), msg = message);
     }
 
     @OnWebSocketClose
     public void onClose(Session user, int statusCode, String reason) throws IOException {
+    	logger.info("User disconected");
     	String username = App.userUsernameMap.get(user);
     	App.userUsernameMap.remove(user);
     }
